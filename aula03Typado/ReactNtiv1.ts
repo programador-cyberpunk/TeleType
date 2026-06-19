588c78ce5b4b0ac3ab9ee0de0e71914ad7e24431
hugo <86883067+programador-cyberpunk@users.noreply.github.com>
2026-06-19 12:27:14 -0300

    Add files via upload

--- aula03Typado/ReactNtiv1.ts
+++ aula03Typado/ReactNtiv1.ts
@@ -71,3 +71,48 @@ function logarTudo<T extends any[]>(...args: T): void {
 }
 
 logarTudo("texto", 42, true);
+
+//map
+const numerosos = [1, 2, 3];
+const dobrados = numerosos.map(n => *2);
+console.log(dobrados);
+
+const usuarios =[
+    {nome: "ana" , idade: 25},
+    {nome: "jao", idade: 30}
+];
+const nombres = usuarios.map(usuarios => usuarios.nome);
+console.log(nombres);
+
+
+//filter
+const numbers = [1,2,3,4,5];
+const pares = numbers.filter(n=> n % 2 === 0);
+console.log(pares);
+
+const usuari = [
+    {nome: "Ana", ativo: true },
+    {nome: "Ze", ativo: false},
+    {nome: "Jao", ativo: true}
+];
+const ativos = usuari.filter(u=> u.ativo);
+console.log(ativos);
+
+//reduce
+const numeros = [1,2,3,4];
+const soma = numeros.reduce((acumulador, atual) => acumulador + atual, 0);
+console.log(soma);
+
+const compras = [
+  { item: "Livro", preco: 30 },
+  { item: "Caneta", preco: 5 },
+  { item: "Caderno", preco: 15 }
+];
+const total = compras.reduce((soma, compra) => soma + compra.preco, 0);
+console.log(total);
+
+//find
+
+const number = [1,2,3,4];
+const encontrado = number.find(n=> n > 2);
+console.log(encontrado);
